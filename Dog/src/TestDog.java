633c74937564ff2379b942caf724645b409b32c1
DELL <DELL@DESKTOP-IF2PEG5>
2021-02-28 09:10:00 +0300

    First Commit

---
+++ Dog/src/TestDog.java
@@ -0,0 +1,23 @@
+
+public class TestDog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Dog Dog1=new Dog("Bulldog","large","light gray",5);
+Dog Dog2=new Dog("Beagle","large","orange",6);
+Dog Dog3=new Dog("GermanShepherd","large","white&orange",6);
+
+System.out.println("The bread of Dog1 is "+Dog1.Bread+" and it's size  is "+Dog1.Size+" and it's color is "+Dog1.Color+" and it's age is "+Dog1.Age);
+System.out.println("The bread of Dog2 is "+Dog2.Bread+" and it's size  is "+Dog2.Size+" and it's color is "+Dog2.Color+" and it's age is "+Dog2.Age);
+System.out.println("The bread of Dog3 is "+Dog3.Bread+" and it's size  is "+Dog3.Size+" and it's color is "+Dog3.Color+" and it's age is "+Dog3.Age);
+	
+
+	}
+public static void Dog(String a,String b,double c,String d){
+			String Eat1 = " Hot Dog";
+		String Run1=" Three hours";
+		double sleep1= 7 ;
+		String Name1 = "Bosh";
+			
+}
+}
