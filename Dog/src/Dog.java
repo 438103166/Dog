9fc46e050c1bfe84d7603c10fc4b63ee7d93f745
DELL <DELL@DESKTOP-IF2PEG5>
2021-02-28 09:26:04 +0300

    First commit

---
+++ Dog/src/Dog.java
@@ -0,0 +1,23 @@
+
+public class Dog {
+String Bread;
+String Size;
+String Color;
+int Age;
+Dog(){
+	
+}
+
+Dog(String newBread,String newSize,String newColor,int newAge ){
+	Bread = newBread;
+	Size =newSize ; 
+	Color=newColor; 
+	Age= newAge;
+}
+
+			
+
+	
+}
+
+
